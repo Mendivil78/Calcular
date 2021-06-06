037989bcbc75ac7d3d5ee98cad80ff60a09fe85a
Alejandro Mendivil <alemendivil-7@hotmail.com>
2021-06-06 15:08:29 +0100

    Añadido funciones resta y division y programa principal by Mendivil78

---
+++ Calcular/CFunciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular
+{
+    class CFunciones
+    {
+        public static decimal Resta(decimal num1, decimal num2)
+        {
+            decimal total = num1 - num2;
+            return (total);
+        }
+        public static decimal Division(decimal dividendo, decimal divisor)
+        {
+            decimal total = 0;
+            if (divisor != 0)
+            {
+                if (dividendo > divisor)
+                {
+                    total = dividendo / divisor;
+                }
+            }
+            return (total);
+        }
+    }
+}
--- Calcular/Program.cs
+++ Calcular/Program.cs
@@ -10,6 +10,16 @@ namespace Calcular
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Proyecto de cálculo");
+            int operador1 = 10;
+            int operador2 = 5;
+            //int suma=CFunciones.Suma(operador1,operador2);
+            decimal resta=CFunciones.Resta(operador1, operador2);
+            //int multiplicacion=CFunciones.Multiplicador(operador1,operador2);
+            decimal division = CFunciones.Division(operador1, operador2);
+            //CFunciones.MostrarResultado(operador1,operador2,suma,resta,multiplicacion, division);
+            Console.WriteLine("Pulsa una tecla para finalizar");
+            Console.ReadKey();
         }
     }
 }
